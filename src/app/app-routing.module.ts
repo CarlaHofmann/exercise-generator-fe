6e696609e95c56ad92a7f4f7de9efa3b69bb7309
CarlaHofmann <carlahfmn@gmail.com>
2022-05-24 22:55:01 +0200

    adapted create-exercise-sheet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import {AuthGuardProfessorService} from "./services/auth-guard-professor.service
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
 import { AddExerciseComponent } from './components/add-exercise/add-exercise.component';
 import { ExerciseSheetsComponent } from './components/exercise-sheets/exercise-sheets.component';
+import { CreateExerciseSheetComponent } from './components/create-exercise-sheet/create-exercise-sheet.component';
 import { ExerciseDatabaseComponent } from './components/exercise-database/exercise-database.component';
 
 
 const routes: Routes = [
     {path: "login", component: LoginComponent},
     {path: "exercise-sheets", component: ExerciseSheetsComponent, canActivate: []},
+    {path: "create-exercise-sheet", component: CreateExerciseSheetComponent, canActivate: []},
     {path: "add-exercise", component: AddExerciseComponent, canActivate: [AuthGuardProfessorService]},
     {path: "exercise-db", component: ExerciseDatabaseComponent, canActivate: []},
     {path: "**", component: DashboardComponent},
