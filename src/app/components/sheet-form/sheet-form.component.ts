5f1b6d7f18b5d07635dc301061bb5615498285d6
CarlaHofmann <carlahfmn@gmail.com>
2022-06-29 09:08:33 +0200

    Adapted sheet-form.component

--- src/app/components/sheet-form/sheet-form.component.ts
+++ src/app/components/sheet-form/sheet-form.component.ts
@@ -9,6 +9,7 @@ import {
     CreateCourseDto,
     Exercise,
     ExerciseApiService,
+    ExerciseDto,
     Sheet,
     SheetApiService,
     SheetDto
@@ -25,6 +26,7 @@ import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
     templateUrl: './sheet-form.component.html',
     styleUrls: ['./sheet-form.component.css']
 })
+
 export class SheetFormComponent implements OnInit, OnDestroy {
     @Input()
     public isCreateSheet: boolean = false;
@@ -45,6 +47,8 @@ export class SheetFormComponent implements OnInit, OnDestroy {
     public courses: Course[] = [];
     public categories: Category[] = [];
     public exercises: Exercise[] = [];
+    public sheetExercises: Exercise[] = [];
+    public numberExercises = 0;
 
     public filteredAuthorNames: String[] = [];
     public filteredCourseNames: String[] = [];
@@ -81,15 +85,21 @@ export class SheetFormComponent implements OnInit, OnDestroy {
 
         this.sheetForm = new FormGroup({
             title: new FormControl("", [Validators.required, Validators.minLength(1)]),
-            courses: new FormControl("", [Validators.required, Validators.minLength(1)]),
-            categories: new FormControl("", [Validators.required, Validators.minLength(1)]),
-            exercises: new FormControl("", [Validators.required, Validators.minLength(1)]),
+            useNumericTitles: new FormControl(false),
+            showSolutions: new FormControl(false),
             isPublished: new FormControl(false)
         });
 
         if (this.isRandomizedSheet) {
+            this.sheetForm.addControl("courses", new FormControl("", [Validators.required, Validators.minLength(1)]));
+            this.sheetForm.addControl("categories", new FormControl("", [Validators.required, Validators.minLength(1)]));
             this.sheetForm.addControl("numberExercises", new FormControl(1, [Validators.required]));
+        } else if (this.isCreateSheet){
+            this.sheetForm.addControl("pageSize", new FormControl(this.dataService.getPageSize()));
         } else {
+            this.sheetForm.addControl("courses", new FormControl("", [Validators.required, Validators.minLength(1)]));
+            this.sheetForm.addControl("categories", new FormControl("", [Validators.required, Validators.minLength(1)]));
+            this.sheetForm.addControl("exercises", new FormControl("", [Validators.required]));
             this.sheetForm.addControl("pageSize", new FormControl(this.dataService.getPageSize()));
         }
 
@@ -160,43 +170,11 @@ export class SheetFormComponent implements OnInit, OnDestroy {
 
     private loadExercises(): void {
         this.exerciseApiService.getAllExercises().subscribe({
-            next: response => {
-                const uniqueAuthors: Author[] = [];
-                response.map(exercise => exercise.author).filter((author: Author) => {
-                    let i = uniqueAuthors.findIndex(a => a.name === author.name);
-                    if (i < 0) {
-                        uniqueAuthors.push(author);
-                    }
-                    return null;
-                })
-                this.authors = uniqueAuthors.sort((a, b) => (a.name < b.name) ? -1 : 1);
-                this.filteredAuthorNames = this.authors.map((author: Author) => author.name);
-
-                const uniqueCourses: Course[] = [];
-                response.flatMap(exercise => exercise.courses).filter((course: Course) => {
-                    let i = uniqueCourses.findIndex(c => c.name === course.name);
-                    if (i < 0) {
-                        uniqueCourses.push(course);
-                    }
-                    return null;
-                })
-                this.courses = uniqueCourses.sort((a, b) => (a.name < b.name) ? -1 : 1);
-
-                const uniqueCategories: Category[] = [];
-                response.flatMap(exercise => exercise.categories).filter((category: Category) => {
-                    let i = uniqueCategories.findIndex(c => c.name === category.name);
-                    if (i < 0) {
-                        uniqueCategories.push(category);
-                    }
-                    return null;
-                })
-                this.categories = uniqueCategories.sort((a, b) => (a.name < b.name) ? -1 : 1);
-
-                this.exercises = response;
-
-
-            },
-            error: error => console.log(error)
+            next: response => this.exercises = response,
+            error: error => {
+                console.log(error);
+                this.displayAlert("Error while loading exercises.");
+            }
         });
     }
 
@@ -223,98 +201,209 @@ export class SheetFormComponent implements OnInit, OnDestroy {
         });
     }
 
-//     public loadSheetExercises(): Exercise[] {
-//             next: response => {
-//                     response.map(exercise => sheet.exercise) => {
-//                         this.sheetExercises = [];
-//                         let i = uniqueAuthors.findIndex(e => e.title === exercise.title);
-//                         if (i < 0) {
-//                             this.sheetExercises.push(exercise);
-//                         }
-//                         return null;
-//                     })
-//                     this.isLoaded = true;
-//             },
-//             error: err => {
-//                 console.log(err);
-//                 this.isLoaded = true;
-//                 this.displayAlert("Error while loading sheet exercises.");
-//             });
-//
-//     }
+    private randomizedExercises(): void {
+        const randomCourseNames = this.sheetDto.courses.map((course: Course) => course.name);
+        const randomCategoryNames = this.sheetDto.categories.map((category: Category) => category.name);
+        const randomExercises = this.exercises.filter((exercise: Exercise) => {
+                    if (randomCourseNames.length) {
+                        return exercise.courses.map((course: Course) => course.name)
+                            .some((courseName: string) => randomCourseNames
+                                .includes(courseName));
+                    }
+                    return true;
+                }).filter((exercise: Exercise) => {
+                    if (randomCategoryNames.length) {
+                        return exercise.categories.map((category: Category) => category.name)
+                            .some((categoryName: string) => randomCategoryNames
+                                .includes(categoryName));
+                    }
+                    return true;
+                });
 
-    get sheetExercises(): Exercise[] {
-        return this.sheet.exercises;
+        this.sheetExercises = [...randomExercises].sort(() => 0.5 - Math.random()).slice(0, this.numberExercises);
     }
 
+    public selectExercise(exercise: Exercise) {
+        if (!this.sheetExercises.includes(exercise)){
+            this.sheetExercises.push(exercise);
+        } else {
+            const indexOfExercise = this.sheetExercises.findIndex((e) => {
+                return e.id === exercise.id;
+            });
+            if (indexOfExercise !== -1) {
+              this.sheetExercises.splice(indexOfExercise, 1);
+            }
+        }
+        this.onFormChange();
+    }
 
-    public getExerciseCourses(courses: Course[]): string {
+    public toggleCheckbox(val: string): void {
+        this.onFormChange();
+        if (val = 'useNumericTitles'){
+                this.sheetDto.useNumericTitles = !this.sheetDto.useNumericTitles;
+        }
+        if (val = 'showSolutions'){
+                this.sheetDto.showSolutions = !this.sheetDto.showSolutions;
+        }
+        if (val = 'isPublished'){
+                this.sheetDto.isPublished = !this.sheetDto.isPublished;
+        }
+    }
+
+    public removeExercise(id: string) {
+        const confirm = window.confirm("Are you sure you want to delete this exercise?");
+        if (confirm) {
+            this.exerciseApiService.deleteExercise(id).subscribe({
+                next: () => {
+                    this.loadExercises();
+                },
+                error: error => {
+                    console.log(error);
+                    this.displayAlert('Error while trying to delete an exercise.');
+                }
+            });
+        }
+    }
+
+    public getCoursesString(courses: Course[]): string {
         return courses.map(course => course.name).join(", ");
     }
 
-    public getExerciseCategories(categories: Category[]): string {
+    public getCategoriesString(categories: Category[]): string {
         return categories.map(category => category.name).join(", ");
     }
 
-    public viewSheetPdf(id: string): void {
-        window.open("sheet/" + id + "/pdf");
+    public booleanToString(value: boolean): string {
+        if(value){
+            return "Yes";
+        }else{
+            return "No";
+        }
     }
 
-    public viewExercisePdf(id: string): void {
-        window.open("exercise/" + id + "/pdf");
-    }
-
-    public toggleCheckbox(): void {
-        this.onFormChange();
-        this.sheetDto.isPublished = !this.sheetDto.isPublished;
+    private exercisesToStringArray(exercises: Exercise[]): string[]{
+        const exercisesStringArray: string[] = [];
+        for (let i=0; i < exercises.length; i++) {
+            exercisesStringArray.push(exercises[i]["id"])
+        }
+        console.log(exercisesStringArray);
+        return exercisesStringArray;
     }
 
     public onFormChange(event?: Event): void {
+        let courses: CreateCourseDto[] = [];
+        let categories: CreateCategoryDto[] = [];
+        let exercises: string[] = [];
+
         if (this.isSubmitted) {
             return;
         }
 
-        let courses: CreateCourseDto[] = [];
-        if (this.sheetForm.controls["courses"].value?.length) {
-            courses = this.sheetForm.controls["courses"].value.map((course: any) => {
-                const newCourse: CreateCourseDto = {name: course.name};
-                return newCourse;
-            });
+        if (this.isRandomizedSheet){
+            if (this.sheetForm.controls["courses"].value?.length) {
+                courses = this.sheetForm.controls["courses"].value.map((course: any) => {
+                    const newCourse: CreateCourseDto = {name: course.name};
+                    return newCourse;
+                });
+            }
+
+            if (this.sheetForm.controls["categories"].value?.length) {
+                categories = this.sheetForm.controls["categories"].value.map((category: any) => {
+                    const newCategory: CreateCategoryDto = {name: category.name};
+                    return newCategory;
+                });
+            }
+
+            this.numberExercises = this.sheetForm.controls["numberExercises"].value;
+
+        } else if (this.isCreateSheet){
+            this.sheetExercises.flatMap(exercise => exercise.courses).filter((course: Course) => {
+                let i = courses.findIndex(c => c.name === course.name);
+                if (i < 0) {
+                    courses.push(course);
+                }
+               return null;
+            })
+
+            this.sheetExercises.flatMap(exercise => exercise.categories).filter((category: Category) => {
+                let i = categories.findIndex(c => c.name === category.name);
+                if (i < 0) {
+                    categories.push(category);
+                }
+               return null;
+            })
+
+        }else{
+            if (this.sheetForm.controls["courses"].value?.length) {
+                courses = this.sheetForm.controls["courses"].value.map((course: any) => {
+                    const newCourse: CreateCourseDto = {name: course.name};
+                    return newCourse;
+                });
+            }
+
+            if (this.sheetForm.controls["categories"].value?.length) {
+                categories = this.sheetForm.controls["categories"].value.map((category: any) => {
+                    const newCategory: CreateCategoryDto = {name: category.name};
+                    return newCategory;
+                });
+            }
         }
 
-        let categories: CreateCategoryDto[] = [];
-        if (this.sheetForm.controls["categories"].value?.length) {
-            categories = this.sheetForm.controls["categories"].value.map((category: any) => {
-                const newCategory: CreateCategoryDto = {name: category.name};
-                return newCategory;
-            });
-        }
+        exercises = this.exercisesToStringArray(this.sheetExercises);
 
         this.sheetDto = {
             title: this.sheetForm.controls["title"].value,
             courses: courses,
             categories: categories,
-            exercises: this.exercises.map(exercise => exercise.id),
+            exercises: exercises,
         }
 
         this.checkUnsavedChanges();
     }
 
     private checkUnsavedChanges(): void {
-        this.dataService.existUnsavedChanges = Boolean(this.sheetForm.controls["title"].value.length ||
-            this.sheetForm.controls["courses"].value &&
-            this.sheetForm.controls["courses"].value.some((course: { name: string }) => course.name.length) ||
-            this.sheetForm.controls["categories"].value &&
-            this.sheetForm.controls["categories"].value.some((category: { name: string }) => category.name.length));
+        if (this.isRandomizedSheet || !this.isCreateSheet){
+            this.dataService.existUnsavedChanges = Boolean(this.sheetForm.controls["title"].value.length ||
+                this.sheetForm.controls["courses"].value &&
+                this.sheetForm.controls["courses"].value.some((course: { name: string }) => course.name.length) ||
+                this.sheetForm.controls["categories"].value &&
+                this.sheetForm.controls["categories"].value.some((category: { name: string }) => category.name.length));
+        }
+//          else {
+//             this.dataService.existUnsavedChanges = Boolean(this.sheetForm.controls["title"].value.length);
+//         }
+
+
     }
 
-    private resetForm(): void {
-        this.sheetForm?.reset();
-        this.dataService.existUnsavedChanges = false;
+    public viewSheetPdf(): void {
+        this.isPdfLoaded = false;
+
+        this.sheetApiService.previewSheetDto(this.sheetDto).subscribe({
+            next: response => {
+                const pdf = new Blob([response], {type: "application/pdf"});
+                this.pdfUrl = URL.createObjectURL(pdf);
+
+                this.isPdfLoaded = true;
+                this.viewportScroller.scrollToPosition([0, 0]);
+            },
+            error: err => {
+                console.log(err);
+                this.displayAlert("Error while trying to get PDF.");
+                this.isPdfLoaded = true;
+            }
+        });
+    }
+
+    public viewExercisePdf(id: string): void {
+        window.open("exercise/" + id + "/pdf");
     }
 
     public onSubmit(goBack: boolean): void {
         if (this.isCreateSheet || this.isCloneSheet) {
+            if (this.isRandomizedSheet){
+                this.randomizedExercises();
+            }
             this.sheetApiService.createSheet(this.sheetDto).subscribe({
                 next: () => {
                     this.isSubmitted = true;
@@ -349,6 +438,11 @@ export class SheetFormComponent implements OnInit, OnDestroy {
         }
     }
 
+    private resetForm(): void {
+        this.sheetForm?.reset();
+        this.dataService.existUnsavedChanges = false;
+    }
+
     public getSanitizedUrl(url: string): SafeResourceUrl {
         return this.sanitizer.bypassSecurityTrustResourceUrl(url);
     }
@@ -358,7 +452,7 @@ export class SheetFormComponent implements OnInit, OnDestroy {
     }
 
     public closeAlert(message: string): void {
-        this.alertMessages = this.alertMessages.filter(m => m !== message);
+         this.alertMessages = this.alertMessages.filter(m => m !== message);
     }
 
     public setPageSize(event: Event): void {
